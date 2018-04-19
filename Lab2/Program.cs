9e07fcb266ec17c16cff670d93bc4cbe0b9e2a95
Margiris <margiris.burakauskas@gmail.com>
2018-04-19 11:43:37 +0300

    GetInteger function done. It asks for input, catches invalid characters and displays a message, returns input if greater than 1 or asks again otherwise

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace Lab2
+﻿using System;
+
+namespace Lab2
 {
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
+            var a = GetInteger();
+
+            Console.WriteLine(a);
+        }
+
+        private static int GetInteger()
+        {
+            var a = 0;
+            
+            while (a <= 1)
+            {
+                Console.Write("Enter an integer greater than 1: ");
+
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input, please try again.");
+                }
+            }
+
+            return a;
         }
     }
 }
