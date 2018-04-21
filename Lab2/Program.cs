4811ef247cfa218e50bdbff6fb5d9b6ce77641e7
Margiris <margiris.burakauskas@gmail.com>
2018-04-21 11:14:53 +0300

    Dynamic version of the Fr function done - Fd. Added array of type long for storing intermediate results of the Fd function.

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -5,17 +5,35 @@ namespace Lab2
 {
     internal static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             var number = GetInteger();
-            var actions = CalculateActions(number);
-            
-            Console.WriteLine("Recursively - " + Fr(number));
-            Console.WriteLine("Dinamicaly - " + Fd(number));
 
+            var intermediateResults = LongArrayWithSingleValue(-1, number);
+
+            Console.WriteLine("Recursively - " + Fr(number));
+            Console.WriteLine("Dinamicaly - " + Fd(number, intermediateResults));
+
+            var actions = CalculateActions(number);
             PrintResults(actions);
         }
-        
+
+        /// <summary>
+        /// Creates and returns an array of type long filled with spedified value.
+        /// </summary>
+        /// <param name="value">The value to write to the array</param>
+        /// <param name="length">The length of the array</param>
+        /// <returns>Newly created array filled with values</returns>
+        private static long[] LongArrayWithSingleValue(long value, int length)
+        {
+            var array = new long[length];
+
+            for (var i = 0; i < length; ++i)
+                array[i] = value;
+
+            return array;
+        }
+
         /// <summary>
         /// Calculates F(n)    = F(n - 2) + 6 * F(n / 5) ^ 2 + 3 * F(n / 6) ^ 2 + n ^ 2 / 5; if n > 1
         ///                    = 2; otherwise
@@ -26,9 +44,7 @@ namespace Lab2
         private static long Fr(long n)
         {
             if (n <= 1)
-            {
                 return 2;
-            }
 
             return Fr(n - 2) +
                    6 * Convert.ToInt64(Math.Pow(Fr(n / 5), 2)) +
@@ -36,11 +52,31 @@ namespace Lab2
                    Convert.ToInt64(Math.Pow(n, 2) / 5);
         }
 
-        private static long Fd(long n)
+        /// <summary>
+        /// Calculates the same function as Fr(long n), but uses an array to store previously calculated results
+        /// in order to save time if the same result is needed.
+        /// </summary>
+        /// <param name="n">Argument of the function</param>
+        /// <param name="results">Previously calculated values of the function</param>
+        /// <returns>Result of the function</returns>
+        // ReSharper disable once SuggestBaseTypeForParameter
+        private static long Fd(long n, long[] results)
         {
-            return 0;
+            if (n <= 1)
+                return 2;
+
+            if (n >= 2)
+                results[n - 2] = (results[n - 2] > -1) ? results[n - 2] : Fd(n - 2, results);
+
+            results[n / 5] = (results[n / 5] > -1) ? results[n / 5] : Fd(n / 5, results);
+            results[n / 6] = (results[n / 6] > -1) ? results[n / 6] : Fd(n / 6, results);
+
+            return results[n - 2] +
+                   6 * Convert.ToInt64(Math.Pow(results[n / 5], 2)) +
+                   3 * Convert.ToInt64(Math.Pow(Fd(n / 6, results), 2)) +
+                   Convert.ToInt64(Math.Pow(n, 2) / 5);
         }
-        
+
         /// <summary>
         /// Asks for input,
         /// catches invalid characters and displays a message,
@@ -68,7 +104,7 @@ namespace Lab2
 
             return a;
         }
-        
+
         /// <summary>
         /// Calculates the smallest amount of actions required to get the given number to 1.
         /// Allowed actions are:
@@ -81,7 +117,7 @@ namespace Lab2
         private static LinkedList<int> CalculateActions(int number)
         {
             var actions = new LinkedList<int>();
-            
+
             while (number > 1)
             {
                 if (number % 3 == 0)
@@ -112,7 +148,7 @@ namespace Lab2
         private static void PrintResults(LinkedList<int> actions)
         {
             if (actions == null) throw new ArgumentNullException(nameof(actions));
-            
+
             Console.WriteLine("Number of actions required - " + actions.Count);
 
             Console.Write("Actions: ");
@@ -120,7 +156,7 @@ namespace Lab2
             {
                 Console.Write(action);
             }
-            
+
             Console.WriteLine();
         }
     }
