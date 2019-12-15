93a356576986008b62ace44af3ce246ef94fbbe9
Margiris <34870816+Margiris@users.noreply.github.com>
2019-12-15 18:56:32 +0200

    Write rule

---
+++ CustomMargirisRule/AllSimpleTypePropertiesShouldBePublic.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.FxCop.Sdk;
+using Microsoft.VisualStudio.CodeAnalysis.Extensibility;
+
+namespace CustomMargirisRule
+{
+    public class AllSimpleTypePropertiesShouldBePublic : BaseIntrospectionRule
+    {
+        public AllSimpleTypePropertiesShouldBePublic() :
+            base(@"AllSimpleTypePropertiesShouldBePublic", "CustomMargirisRule.Rules",
+                typeof(AllSimpleTypePropertiesShouldBePublic).Assembly)
+        {
+        }
+
+        public override ProblemCollection Check(Parameter parameter)
+        {
+            if (parameter.Type.IsPrimitive && parameter.Type.IsNonPublic)
+            {
+                var resolution = GetResolution(parameter.Name.Name);
+                Problems.Add(new Problem(resolution, parameter)
+                {
+                    Certainty = 100,
+                    FixCategory = FixCategories.NonBreaking,
+                    MessageLevel = MessageLevel.Warning
+                });
+            }
+
+            return Problems;
+        }
+    }
+}
