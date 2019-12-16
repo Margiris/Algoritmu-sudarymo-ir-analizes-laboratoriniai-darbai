2dc709586a48a73a50780f2684501811606e291c
Margiris <34870816+Margiris@users.noreply.github.com>
2019-12-17 00:05:43 +0200

    Remove FxCop rule project completely, implement Roslyn analyzer

--- MargirisRule/MargirisRule/MargirisRuleAnalyzer.cs
+++ MargirisRule/MargirisRule/MargirisRuleAnalyzer.cs
@@ -15,42 +15,63 @@ namespace MargirisRule
     {
         public const string DiagnosticId = "MargirisRule";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private static readonly string Title = "Accessibility rule violation";
+        private static readonly string MessageFormat = "Primitive non-const field {0} should be public";
+        // private static readonly string MessageFormat = "{0}";
+        private static readonly string Description = "No need to hide simple things";
+        private const string Category = "Visibility";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
+            Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(Rule); }
+        }
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(AnalyzeProperty, SyntaxKind.FieldDeclaration);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static void AnalyzeProperty(SyntaxNodeAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var field = (IFieldSymbol) context.ContainingSymbol;
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            if (!field.IsConst && field.DeclaredAccessibility != Accessibility.Public && IsFieldPrimitive(field))
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
+                var diagnostic = Diagnostic.Create(Rule, field.Locations[0], field.Name);
                 context.ReportDiagnostic(diagnostic);
             }
+
+            // var diagnostic = Diagnostic.Create(Rule, field.Locations[0],
+            //     IsFieldPrimitive(field) + ", " + field.Type.ToDisplayString());
+            //
+            // context.ReportDiagnostic(diagnostic);
         }
 
-        private static void AnalyzeProperty(AnalysisContext a)
+        internal static bool IsFieldPrimitive(IFieldSymbol field)
         {
+            var primitiveFieldTypes = new List<string>
+            {
+                "bool",
+                "byte",
+                "sbyte",
+                "char",
+                "decimal",
+                "double",
+                "float",
+                "int",
+                "uint",
+                "long",
+                "ulong",
+                "object",
+                "short",
+                "ushort",
+                "string"
+            };
 
+            return primitiveFieldTypes.Contains(field.Type.ToDisplayString());
         }
     }
 }
